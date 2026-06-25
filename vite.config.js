f185f1b46ae5798c978a66f602a153f5d68f9382
Pipslordy <adurobeek@gmail.com>
2026-06-25 14:11:58 +0300

    feat: add custom SVG icon for PWA

--- vite.config.js
+++ vite.config.js
@@ -17,19 +17,9 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'https://cdn-icons-png.flaticon.com/512/2921/2921222.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'https://cdn-icons-png.flaticon.com/512/2921/2921222.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'https://cdn-icons-png.flaticon.com/512/2921/2921222.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: '/icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
             purpose: 'any maskable'
           }
         ]
